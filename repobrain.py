e1b73ee8ae147a1e3ada409c4422c3dd162f7da5
NRB-XZO <78638685+NRB-XZO@users.noreply.github.com>
2022-06-27 13:31:55 +0300

    Update repobrain.py

--- repobrain.py
+++ repobrain.py
@@ -4,6 +4,8 @@
 import keyboard
 from python_imagesearch.imagesearch import imagesearch
 from datetime import datetime
+import selenium
+from selenium import webdriver
 import os
 from requests import post
 import pyautogui
@@ -14,6 +16,11 @@ from random import randint
 from os import system
 from PyQt5 import QtWidgets,QtGui
 import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep, strftime
+from random import randint
+import pandas as pd
 username = "NRB"
 password = randint(10000000,100000000000000)
 while True:
@@ -67,50 +74,10 @@ class Pencere(QtWidgets.QWidget):
         self.show()
 
     def click(self):
-        webbrowser.open("web.whatsapp.com",new=0,autoraise=True)
-        id = str(pyautogui.prompt(text='Lütfen telegram id girin', title='NRB SECURİTY' , default=''))
-        time_x = 0
-        analys_text = open(
-            "analiz{}_{}_{}_{}.txt".format(datetime.now().day, datetime.now().hour, datetime.now().minute,
-                                           datetime.now().second), "w")
-        try:
-            while True:
-                if imagesearch(image="active.PNG")[0] != -1:
-                    time_x = 0
-                    analys_text.write("Giriş yapıldı: {}:{}\n".format(datetime.now().hour, datetime.now().minute))
-                    post("https://api.telegram.org/bot5051736797:AAEw9X9SWyJKoAsgJhbq49i70nqZfHp8F5w/sendMessage",
-                         data={"chat_id": id, "text": "WhatsApp-Giriş yapıldı. BO1"})
-                    while True:
-                        if imagesearch(image="active.PNG")[0] != -1:
-                            print("Online - {}".format(time_x))
-                            time_x += 1
-                            time.sleep(1)
-                        else:
-                            analys_text.write(
-                                "Aktif kalındı: {} dk {} sn\n".format(time_x // 60, time_x - (time_x // 60) * 60))
-                            analys_text.write(
-                                "Çıkış yapıldı: {}:{}\n".format(datetime.now().hour, datetime.now().minute))
-                            post(
-                                "https://api.telegram.org/bot5051736797:AAEw9X9SWyJKoAsgJhbq49i70nqZfHp8F5w/sendMessage",
-                                data={"chat_id": id,
-                                      "text": "{} dk {} sn boyunca whatsapp'ta aktif kaldı ve çıkış yaptı. BO2".format(
-                                          time_x // 60, time_x - (time_x // 60) * 60)})
-                            time_x = 0
-                            break
-                elif imagesearch(image="active.PNG")[0] == -1:
-                    time.sleep(1)
-                    time_x += 1
-                    print("Offline - {}".format(time_x))
+        browser = webdriver.Firefox()
 
-        except:
-            post("https://api.telegram.org/bot5051736797:AAEw9X9SWyJKoAsgJhbq49i70nqZfHp8F5w/sendMessage",
-                 data={"chat_id": id, "text": "Bir hata oluştu ! BO3"})
-        finally:
-            analys_text.close()
-            post("https://api.telegram.org/bot5051736797:AAEw9X9SWyJKoAsgJhbq49i70nqZfHp8F5w/sendMessage",
-                 data={"chat_id": id, "text": "Txt log dosyası kaydedildi mevcut konuma: {}".format(os.getcwd())})
-
-
+        time.sleep(10)
+        browser.close()
 app = QtWidgets.QApplication(sys.argv)
 pencere = Pencere()
 sys.exit(app.exec_())
